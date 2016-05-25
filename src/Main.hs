bb1b7a30caa6b22b8b3f05cf501a06f88bef5f5b
PRudnik <pawelrudnik92@o2.pl>
2016-05-25 23:52:21 +0200

    Wczytywanie pliku z poziomu konsoli

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,7 @@
 import System.Environment
 
 main = do
-  args <- getArgs
-  if length args < 1
-    then error "usage: Pyramids <filename>"
-  else do
-    let filename = last args
-    content <- readFile filename
-    putStrLn content
+  putStrLn "Wpisz nazwę pliku, z którego ma być wczytana łamigłówka:"
+  filename <- getLine
+  content <- readFile filename
+  putStrLn content
