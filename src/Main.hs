1bc614e34785e830345aa55af733c0d708007bce
PRudnik <pawelrudnik92@o2.pl>
2016-05-26 01:40:32 +0200

    Czekanie na użytkownika

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,16 @@
-import System.Environment
+import Pyramids
 
 main = do
+  
   putStrLn "Wpisz nazwę pliku, z którego ma być wczytana łamigłówka:"
   filename <- getLine
   content <- readFile filename
-  putStrLn content
+  
+  let input = read content::Piramidy
+  let output = createEmptyBoard input
+
+  putStrLn $ show output
+
+  putStrLn "Wciśnij klawisz enter aby zakończyć"
+  key <- getChar
+  return key
